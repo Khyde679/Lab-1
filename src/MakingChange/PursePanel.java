f11090d81b20197ccb3a94743f01e7ea87f8aed0
Kaden Hyde <khyde@cub.uca.edu>
2025-01-23 22:46:26 -0600

    test 2

--- src/MakingChange/PursePanel.java
+++ src/MakingChange/PursePanel.java
@@ -4,30 +4,35 @@ import javax.swing.*;
 import java.awt.*;
 
 public class PursePanel extends JPanel {
-    private Purse purse;
+    private Purse newPurse;
 
     PursePanel() {
-        purse = new Purse();
+        newPurse = new Purse();
         this.setBackground(Color.RED);
         this.setPreferredSize(new Dimension(700, 700));
     }
 
     public void updatePurse(Purse newPurse) {
-        this.purse = newPurse;
-        removeAll();
+        this.newPurse = newPurse;
+
+        for (Component component : this.getComponents()) {
+            this.remove(component);
+        }
+
+        for (Denomination type : newPurse.cash.keySet()) {
+            for (int i = 1; i < newPurse.cash.get(type); i++) {
+                ImageIcon icon = new ImageIcon(type.img());
+                JLabel label = new JLabel(icon);
+                this.add(label);
+            }
+        }
         revalidate();
         repaint();
+        newPurse.cash.clear();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-        for (Denomination type : purse.cash.keySet()) {
-            ImageIcon icon = new ImageIcon(type.img());
-            JLabel label = new JLabel(icon);
-            this.add(label);
-        }
-
 
     }
 }
